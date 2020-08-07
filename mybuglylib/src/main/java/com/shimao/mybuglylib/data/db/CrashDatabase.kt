6f1eed991be002477496a265cef23d05c4a9dabb
JianZhao <517669211@qq.com>
2020-08-07 11:31:23 +0800

    提高数据库版本，增加升级清空策略

--- mybuglylib/src/main/java/com/shimao/mybuglylib/data/db/CrashDatabase.kt
+++ mybuglylib/src/main/java/com/shimao/mybuglylib/data/db/CrashDatabase.kt
@@ -24,6 +24,7 @@ abstract class CrashDatabase:RoomDatabase() {
                 "crashdatabase"
             )
                 .allowMainThreadQueries()
+                .fallbackToDestructiveMigration()
                 .build()
         }
 
